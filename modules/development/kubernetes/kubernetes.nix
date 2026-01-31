6ba78a483a6a618f1d1aebd54ff25fac44db3ea4
Guilherme Danno <guilherme.danno@gmail.com>
2026-01-31 18:13:25 -0300

    feat: partial krewfile config

--- modules/development/kubernetes/kubernetes.nix
+++ modules/development/kubernetes/kubernetes.nix
@@ -11,4 +11,23 @@
       };
     };
   };
+
+  # TODO: import krewfile HM module
+  # FIXME: krewfile module dependency on ssh because of the git ssh override
+  flake.homeModules.krew = {
+    config,
+    namespace,
+    pkgs,
+    ...
+  }: {
+    config = {
+      home.sessionVariables = {
+        KREW_ROOT = "${config.home.homeDirectory}/.config/krew";
+      };
+
+      home.sessionPath = [
+        "${config.home.sessionVariables.KREW_ROOT}/bin"
+      ];
+    };
+  };
 }
