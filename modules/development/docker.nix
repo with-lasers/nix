6979e1cd0cedbe329582d6a0ddfcd7e1d0c255be
Guilherme Danno <guilherme.danno@gmail.com>
2026-02-01 03:22:46 -0300

    feat: extract more scripts

--- modules/development/docker.nix
+++ modules/development/docker.nix
@@ -16,6 +16,33 @@
         dcu = "docker compose up -d";
         lzd = "lazydocker";
       };
+
+      home.packages = with pkgs; let
+        entrypoints = map (cmd:
+          writeShellApplication {
+            name = "${cmd}-it";
+            text = ''docker run --rm -it --entrypoint ${cmd} "$@"'';
+          }) ["bash" "ash" "sh" "node"];
+
+        latestTags = map (type: (writeShellApplication {
+          name = "latest-${type}-tag";
+          text = ''latest-tag "$1" "${type}"'';
+        })) ["snapshot" "release"];
+      in
+        lib.concatLists [
+          latestTags
+          entrypoints
+          [
+            (writeShellApplication {
+              name = "latest-tag";
+              runtimeInputs = [regctl ripgrep];
+              text = ''
+                registry="''${IMAGE_REGISTRY:-docker.io}"
+                regctl tag ls "$registry/$1" | rg "$2-\\d{14}\$" | sort -V | tail -n1
+              '';
+            })
+          ]
+        ];
     };
   };
 }
