31504c3f8680ccbd164545ba371aa63c5a707eb5
Guilherme Danno <guilherme.danno@gmail.com>
2026-08-02 18:25:06 -0300

    chore: merge lib options

--- modules/flake-parts/flake-parts.nix
+++ modules/flake-parts/flake-parts.nix
@@ -1,10 +1,19 @@
-{inputs, ...}: {
-  flake-file.description = "My public modules";
-  flake-file.inputs = {
+{
+  inputs,
+  lib,
+  ...
+}: {
+  config.flake-file.description = "My public modules";
+  config.flake-file.inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
     flake-parts.url = "github:hercules-ci/flake-parts";
     import-tree.url = "github:vic/import-tree";
     systems.url = "github:nix-systems/default";
+  };
+
+  options.flake.lib = lib.mkOption {
+    type = lib.types.attrsOf lib.types.anything;
+    default = {};
   };
 
   imports = [inputs.flake-parts.flakeModules.modules];
