c467892abbbf62c7a99866702f0347b7075585db
Guilherme Danno <guilherme.danno@gmail.com>
2026-08-03 00:58:42 -0300

    fix: use gh token when nix flake update

--- modules/shell/nix.nix
+++ modules/shell/nix.nix
@@ -9,13 +9,20 @@
       ${namespace}.shell.aliases = {
         hms = "nh home switch $NIXOS_REPO";
         nup = "nh os switch $NIXOS_REPO";
-        nfu = "nix flake update";
+        nfu = "nix-flake-update";
       };
 
       home.homeDirectory = "/home/${config.home.username}";
       home.packages = with pkgs; [
         nh
         devenv
+        (writeShellApplication {
+          name = "nix-flake-update";
+          runtimeInputs = [gh];
+          text = ''
+            NIX_CONFIG="access-tokens = github.com=$(gh auth token)" nix flake update
+          '';
+        })
       ];
     };
   };
